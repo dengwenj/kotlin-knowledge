bbee3a7be785f92056d384fe008efca38d2c81d3
朴睦 <927561237@qq.com>
2024-08-09 15:37:05 +0800

    数组

---
+++ src/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) {
+        String str = "1234";
+        char c = str.charAt(0);
+        System.out.println(c);
+    }
+}
---
+++ src/datatype/array.kt
@@ -0,0 +1,10 @@
+package datatype
+
+fun main() {
+    var arr = intArrayOf(1, 2, 4, 5, 6, 7, 8, 9, 10);
+    // 创建数组的方式
+    var arrStr: Array<String> = arrayOf("朴睦", "李雷")
+    for (s in arrStr) {
+        println(s)
+    }
+}
